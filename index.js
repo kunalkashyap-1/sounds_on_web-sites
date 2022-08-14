06f6e1cec056310b5d5c85e244009e6dbd09d566
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-08-14 17:48:20 +0530

    added keydown event

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-let numberOfButtons=document.querySelectorAll(".drum").length;
+let numberOfButtons = document.querySelectorAll(".drum").length;
 
-function play_audio(){
-    var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
+function play_sound(key){
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            let kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+
+        case "k":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "l":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        default:
+            console.log("invalid")
+            break;
+    }
+}
+
+function play_audio_click() {
+    var key = this.getInnerHTML();
+    play_sound(key);
 }
 
 for (let i = 0; i < numberOfButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",play_audio);
+    document
+        .querySelectorAll(".drum")
+    [i].addEventListener("click", play_audio_click);
 }
+
+document.addEventListener("keydown",function(event){
+    var key=event.key;
+    play_sound(key);
+});
